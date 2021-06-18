04dce31f0fc9f5e1a53105dae896a0d6c37f5cea
andrew <neon.duncan@gmail.com>
2021-06-18 14:25:27 -0500

    commit tree 8ccba8d35391b2ba8df731ef449b4a4aa9cd1e6c

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = 'Like! ♡'
+const FULL_HEART = 'Like! ♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = () => document.querySelector("#modal")
+const likeButtons = () => document.querySelectorAll("li")
+const hearts = () => document.querySelectorAll(".like-glyph")
 
+document.addEventListener("DOMContentLoaded", () => {
+  addRemoveErrorMessage()
+  likeButtons().forEach (line => {
+    line.addEventListener("click", event => {
+      mimicServerCall()
+      .then( data => data)
+      .catch(error => addRemoveErrorMessage())
+      changeLikeStatus(event)
+    })
+  })
+  
+})
+
+const changeLikeStatus = (event) => {
+  if (event.target.classList.contains("activated-heart")) {
+    event.target.classList.remove("activated-heart")
+  } else {
+    event.target.classList.add("activated-heart")
+  } 
+  event.target.innerText = (event.target.innerText === EMPTY_HEART ? FULL_HEART : EMPTY_HEART)
+}
+
+
+//adds error message if not displayed and displays error message if being displayed
+const addRemoveErrorMessage= () =>{
+  if (errorModal().classList.contains("hidden")) {
+    errorModal().classList.remove("hidden") 
+  } else {
+    errorModal().classList.add("hidden") 
+  }
+}
 
 
 //------------------------------------------------------------------------------
